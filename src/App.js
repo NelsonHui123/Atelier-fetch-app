e2b4781f62d0096e1112c5a6d66fd97b22a1a0a8
Nelson Hui <nelson.hui123@gmail.com>
2018-10-08 14:39:08 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import  GenerateEmployee  from  './GenerateEmployee';
+import  DisplayEmployee  from  './DisplayEmployee';
+
+const  sampleEmployee = {
+  gender:  'male',
+  name: {
+      title:  'mr',
+      first:  'mathys',
+      last:  'aubert'
+  },
+  location: {
+      street:  '9467 rue gasparin',
+      city:  'perpignan',
+      postcode:  '90208'
+  },
+  email:  'mathys.aubert@example.com',
+  picture: {
+      medium:  'https://randomuser.me/api/portraits/med/men/66.jpg'
+  }
+};
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee:  sampleEmployee
+    };
+  }
+
+  getEmployee() {
+    // Récupération de l'employé via fetch
+    fetch("https://randomuser.me/api?nat=fr")
+      .then(response  =>  response.json())
+      .then(data  => {
+        // Une fois les données récupérées, on va mettre à jour notre state avec les nouvelles données
+        this.setState({
+          employee:  data.results[0],
+        });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GenerateEmployee selectEmployee={() =>  this.getEmployee()} />
+        <DisplayEmployee  employee={this.state.employee}  />
       </div>
     );
   }
